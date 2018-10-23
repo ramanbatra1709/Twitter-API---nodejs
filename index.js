8349e2153998386bbea7f5a7f7e39dd574972759
Raman Batra <raman.batra1709@gmail.com>
2018-10-23 21:42:13 +0530

    Add body-parser middlewares

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const twit = require('twit');
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.listen(3000);
