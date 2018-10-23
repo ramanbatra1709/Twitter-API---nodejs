f438364e511de710e7bc9cd7e22176caca95888b
Raman Batra <raman.batra1709@gmail.com>
2018-10-23 22:10:35 +0530

    Add search api route

--- index.js
+++ index.js
@@ -9,4 +9,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.get('/tweets/:search', function(req, res)   {
+    console.log(req.params.search);
+    res.json({
+        'status': 'Success'
+    });
+});
+
 app.listen(3000);
