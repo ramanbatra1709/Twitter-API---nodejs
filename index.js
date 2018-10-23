4d4f7c3c6c8119c7883a4f6c48925fdef7cfb011
Raman Batra <raman.batra1709@gmail.com>
2018-10-23 21:09:55 +0530

    Create static middleware

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
-app.get('/', function(req, res) {
-    res.send('Hello, World!');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000);
